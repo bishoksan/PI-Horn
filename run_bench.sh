385935e25f6eb509e2bcd23975a6d56dd5a4d444
Bishoksan Kafle <bishoksan@gmail.com>
2018-11-27 18:04:52 +1100

    Update run_bench.sh

--- run_bench.sh
+++ run_bench.sh
@@ -2,12 +2,11 @@
 
 #the timeout command works for OSX. The results will be stored in result.txt.
 progs=$1 # folder containing benchmarks
-progs=$1
-START1=$(date +%s000)
+START=$(date +%s000)
 for file in $progs/*.pl
 do
   gtimeout  5m 	pihorn -pe  "$file"
 done
-END1=$(date +%s000)
-DIFF1=$(( $END1 - $START1 ))
-echo  " totaltime = $DIFF1 miliseconds"   >>  "result.txt"
+END=$(date +%s000)
+DIFF=$(( $END - $START ))
+echo  " totaltime = $DIFF miliseconds"   >>  "result.txt"
